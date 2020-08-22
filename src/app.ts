3ebbd216a3ad085bbf12242963811eb88f1ebb19
Athnei <atthnei@Athnei.localdomain>
2020-08-23 00:32:04 +0200

    solving captcha

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,41 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
+const puppeteer = require('puppeteer');
+// import { createWorker } from 'tesseract.js';
+import { GetCaptcha } from './tesseract';
 
-const app: Application = express();
+(async () => {
+  const browser = await puppeteer.launch()
+  try {
+    const page = await browser.newPage()
+    const navigationPromise = page.waitForNavigation()
 
-const port = 3000
+    await page.goto('https://rarbgtorrents.org/torrents.php', { waitUntil: 'networkidle2' })
+    await page.setViewport({ width: 1366, height: 357 })
+    await page.waitForSelector('table > tbody > tr:nth-child(2) > td:nth-child(2) > img')
+    await page.waitFor(1000);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    const element = await page.$('body > form > div > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2) > img')
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    await element.screenshot({ path: 'images/code.png' })
+
+    const data = await GetCaptcha();
+
+    console.log(data);
+
+    await page.type('table #solve_string', data);
+
+    await page.waitForSelector('body > table:nth-child(6) > tbody > tr > td:nth-child(2) > div > table > tbody > tr:nth-child(2) > td > div:nth-child(1) > table')
+
+    let tit = await page.evaluate(() => {
+      let title = document.querySelector('body > table:nth-child(6) > tbody > tr > td:nth-child(2) > div > table > tbody > tr:nth-child(2) > td > div:nth-child(1) > table > tbody > tr > td:nth-child(1) > a')?.getAttribute('title');
+
+      return title;
+    })
+    console.log(tit);
+    await page.screenshot({ path: 'images/torrents.png' })
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await browser.close();
+  }
+})();
---
+++ src/tesseract.ts
@@ -0,0 +1,18 @@
+import { createWorker } from 'tesseract.js';
+
+const worker = createWorker({
+    //   logger: m => console.log(m)
+});
+
+export async function GetCaptcha(location: string = 'images/code.png') {
+
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(location);
+    // console.log(text);
+    await worker.terminate();
+
+    return text
+
+}
